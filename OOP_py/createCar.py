69b88a1299a84941d2bc4743faf246d0811a11f6
John <vanya432134@gmail.com>
2020-06-03 08:36:58 +0300

    change commit

--- OOP_py/createCar.py
+++ OOP_py/createCar.py
@@ -1,19 +1,3 @@
-class Car: #Основной классс машины
-    speed = '220ml/h'
-    model = 'Mercedes'
-    color = 'Green'
-    def createCar(self):
-        print('Создана машина:\n' + 'Модель - ' + self.model + '\nЦвет - ' + self.color + '\nСкорость - ' + self.speed)
-    def __del__(self):
-        print('_' + self.speed + self.model, self.color, "удален из памяти")
-
-class miniCar(Car): #Дочерний класс машины
-    carType = 'mini'        
-    def createCar(self):
-        print('Создана машина мини:\n' + 'Модель - ' + self.model + '\nЦвет - ' + self.color + '\nСкорость - ' + self.speed + '\nТип - ' + self.carType)
-    def __del__(self):
-        print('_' + self.speed, self.model,  self.color, self.carType, "удален из памяти")
-
 def showInfoCar(point): #Функция
     if point == 1:
         car = Car()
